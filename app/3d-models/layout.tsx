0d981c896cdacfc7bd122fa18dd68dc01a8607d9
Zaid Qtaish <enderzaide@gmail.com>
2026-01-19 20:39:58 +0300

    refactor: split ModelSidebar into a seperate component and shifted it slightly down

--- app/3d-models/layout.tsx
+++ app/3d-models/layout.tsx
@@ -1,28 +1,11 @@
 "use client";
 
-import { getAllCategories } from "../lib/catergories"
-import NavButton from "../components/NavButton"
-import { usePathname } from "next/navigation"
+import ModelsSidebar from "../components/ModelsSidebar";
 
 export default function ModelsNavigation({ children }: { children: React.ReactNode }) {
-    const pathname = usePathname();
-    const categories = getAllCategories();
     return (
         <main className="max-w-7xl mx-auto md:flex md:mx-0 md:gap-20 md:px-10 md:max-w-full">
-            <aside className="w-full md:w-48 md:sticky md:top-55 md:h-fit">
-                <nav className="flex gap-4 overflow-x-auto md:flex-col md:gap-3 md:py-4">
-                    <NavButton href="/3d-models" isActive={pathname === "/3d-models"} variant="sidebar">ALL</NavButton>
-                    {categories.map(category =>
-                        <NavButton
-                            key={category.slug}
-                            href={`/3d-models/categories/${category.slug}`}
-                            isActive={pathname === `/3d-models/categories/${category.slug}`}
-                            variant="sidebar"
-                        >
-                            {category.displayName.toUpperCase()}
-                        </NavButton>)}
-                </nav>
-            </aside>
+            <ModelsSidebar />
             {children}
         </main>
     )
---
+++ app/components/ModelsSidebar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import NavButton from "../components/NavButton"
+import { getAllCategories } from "../lib/catergories"
+import { usePathname } from "next/navigation"
+
+export default function ModelsSidebar() {
+    const pathname = usePathname();
+    const categories = getAllCategories();
+
+    return (
+        <aside className="w-full md:w-48 md:sticky md:top-65 md:h-fit">
+            <nav className="flex gap-4 overflow-x-auto md:flex-col md:gap-3 md:py-4">
+                <NavButton href="/3d-models" isActive={pathname === "/3d-models"} variant="sidebar">ALL</NavButton>
+                {categories.map(category =>
+                    <NavButton
+                        key={category.slug}
+                        href={`/3d-models/categories/${category.slug}`}
+                        isActive={pathname === `/3d-models/categories/${category.slug}`}
+                        variant="sidebar"
+                    >
+                        {category.displayName.toUpperCase()}
+                    </NavButton>)}
+            </nav>
+        </aside>
+    )
+}
