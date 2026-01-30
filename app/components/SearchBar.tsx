f213a8cbcd4d3a4c439b500fc525095db5ad31ba
Zaid Qtaish <enderzaide@gmail.com>
2026-01-30 21:29:08 +0300

    chore: turned searchbar form element into Next.js Form for client-side navigation

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,6 +1,8 @@
+import Form from "next/form";
+
 export default function SearchBar({ defaultValue }: { defaultValue?: string }) {
     return (
-            <form className="px-3 md:px-0 md:max-w-xl">
+            <Form action="" className="px-3 md:px-0 md:max-w-xl">
                 <input
                     type="text"
                     name="query"
@@ -9,6 +11,6 @@ export default function SearchBar({ defaultValue }: { defaultValue?: string }) {
                     defaultValue={defaultValue}
                     className="w-full md:w-70 py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
                 />
-            </form>
+            </Form>
     );
 }
